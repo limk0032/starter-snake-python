d38a2885b14b3be446e36b98e08fb1bcc7c2f4df
limk0032 <limk0032@gmail.com>
2021-10-05 00:43:05 +0800

    update

--- server.py
+++ server.py
@@ -1,77 +1,183 @@
-import logging
-import os
+import random
+from typing import List, Dict
+import math
 
-from flask import Flask
-from flask import request
+"""
+This file can be a nice home for your move logic, and to write helper functions.
 
-import server_logic
+We have started this for you, with a function to help remove the 'neck' direction
+from the list of possible moves!
+"""
 
 
-app = Flask(__name__)
-
-
-@app.get("/")
-def handle_info():
+def avoid_my_neck(my_head: Dict[str, int], my_body: List[dict], possible_moves: List[str]) -> List[str]:
     """
-    This function is called when you register your Battlesnake on play.battlesnake.com
-    See https://docs.battlesnake.com/guides/getting-started#step-4-register-your-battlesnake
+    my_head: Dictionary of x/y coordinates of the Battlesnake head.
+            e.g. {"x": 0, "y": 0}
+    my_body: List of dictionaries of x/y coordinates for every segment of a Battlesnake.
+            e.g. [ {"x": 0, "y": 0}, {"x": 1, "y": 0}, {"x": 2, "y": 0} ]
+    possible_moves: List of strings. Moves to pick from.
+            e.g. ["up", "down", "left", "right"]
 
-    It controls your Battlesnake appearance and author permissions.
-    For customization options, see https://docs.battlesnake.com/references/personalization
-
-    TIP: If you open your Battlesnake URL in browser you should see this data.
+    return: The list of remaining possible_moves, with the 'neck' direction removed
     """
-    print("INFO")
-    return {
-        "apiversion": "1",
-        "author": "",  # TODO: Your Battlesnake Username
-        "color": "#888888",  # TODO: Personalize
-        "head": "default",  # TODO: Personalize
-        "tail": "default",  # TODO: Personalize
-    }
+    print(f'my_head:{my_head}, my_body:{my_body}')
+    if {"x": my_head["x"], "y": my_head["y"] + 1} in my_body and 'up' in possible_moves:
+        possible_moves.remove("up")
+    if {"x": my_head["x"], "y": my_head["y"] - 1} in my_body and 'down' in possible_moves:
+        possible_moves.remove("down")
+    if {"x": my_head["x"] + 1, "y": my_head["y"]} in my_body and 'right' in possible_moves:
+        possible_moves.remove("right")
+    if {"x": my_head["x"] - 1, "y": my_head["y"]} in my_body and 'left' in possible_moves:
+        possible_moves.remove("left")
+    return possible_moves
 
 
-@app.post("/start")
-def handle_start():
+def avoid_border(my_head: Dict[str, int], my_body: List[dict], possible_moves: List[str], board_height: int, board_width: int) -> List[str]:
+    print(f'in avoid_border, board_height:{board_height}, board_width:{board_width}, my_head:{my_head}')
+    if board_height - 1 == my_head["y"] and 'up' in possible_moves:
+        possible_moves.remove("up")
+        print('remove up')
+    if 0 == my_head["y"] and 'down' in possible_moves:
+        possible_moves.remove("down")
+        print('remove down')
+    if board_width - 1 == my_head["x"] and 'right' in possible_moves:
+        possible_moves.remove("right")
+        print('remove right')
+    if 0 == my_head["x"] and 'left' in possible_moves:
+        possible_moves.remove("left")
+        print('remove left')
+    return possible_moves
+
+
+def get_nearest_food(my_head: Dict[str, int], foods: List[dict]):
+    print(f'my_head:{my_head}, foods:{foods}')
+    length_food_dict = {}
+    for food in foods:
+        distance = math.sqrt((food['x'] - my_head['x']) ** 2 + (food['y'] - my_head['y']) ** 2)
+        length_food_dict[distance] = food
+    length_list = length_food_dict.keys()
+    min_length = min(length_list)
+    return length_food_dict[min_length]
+
+
+def combine_preferred_directions_with_possible_moves(preferred_directions: List[str], possible_moves: List[str]):
+    result = []
+    for preferred_direction in preferred_directions:
+        if preferred_direction in possible_moves:
+            result.append(preferred_direction)
+    return result
+
+
+def get_preferred_directions_to_food(my_head: Dict[str, int], food: Dict[str, int], possible_moves: List[str]):
+    preferred_direction = []
+    if food['x'] == my_head['x'] and food['y'] < my_head['y']:
+        preferred_direction = combine_preferred_directions_with_possible_moves(['down', 'left', 'right', 'up'], possible_moves)
+    elif food['x'] == my_head['x'] and food['y'] > my_head['y']:
+        preferred_direction = combine_preferred_directions_with_possible_moves(['up', 'left', 'right', 'down'], possible_moves)
+    elif food['x'] < my_head['x'] and food['y'] == my_head['y']:
+        preferred_direction = combine_preferred_directions_with_possible_moves(['left', 'up', 'down', 'right'], possible_moves)
+    elif food['x'] > my_head['x'] and food['y'] == my_head['y']:
+        preferred_direction = combine_preferred_directions_with_possible_moves(['right', 'up', 'down', 'left'], possible_moves)
+    elif food['x'] < my_head['x'] and food['y'] < my_head['y']:
+        preferred_direction = combine_preferred_directions_with_possible_moves(['down', 'left', 'up', 'right'], possible_moves)
+    elif food['x'] < my_head['x'] and food['y'] > my_head['y']:
+        preferred_direction = combine_preferred_directions_with_possible_moves(['up', 'left', 'down', 'right'], possible_moves)
+    elif food['x'] > my_head['x'] and food['y'] < my_head['y']:
+        preferred_direction = combine_preferred_directions_with_possible_moves(['down', 'right', 'up', 'left'], possible_moves)
+    elif food['x'] > my_head['x'] and food['y'] > my_head['y']:
+        preferred_direction = combine_preferred_directions_with_possible_moves(['up', 'right', 'down', 'left'], possible_moves)
+    return preferred_direction
+
+
+def get_number_of_connected_tile(start: Dict[str, int], my_body: List[dict], other_snakes: List[List[dict]], board_height: int, board_width: int):
+    processed_tiles = []
+    not_accessible_tiles = my_body
+    for other_snake in other_snakes:
+        not_accessible_tiles = not_accessible_tiles + other_snake['body']
+    number_of_connected_tile = get_number_of_connected_tile_recursively(start, not_accessible_tiles, board_height, board_width, processed_tiles)
+    return number_of_connected_tile
+
+
+def get_number_of_connected_tile_recursively(start: Dict[str, int], not_accessible_tiles: List[dict], board_height: int, board_width: int, processed_tiles: List[dict]):
+    if start in processed_tiles:
+        return 0
+    if start in not_accessible_tiles:
+        return 0
+    if start['x'] < 0 or start['x'] == board_width:
+        return 0
+    if start['y'] < 0 or start['y'] == board_height:
+        return 0
+    processed_tiles.append(start)
+    result = 1
+    result = result + get_number_of_connected_tile_recursively({"x": start["x"] + 1, "y": start["y"]}, not_accessible_tiles, board_height, board_width, processed_tiles)
+    result = result + get_number_of_connected_tile_recursively({"x": start["x"], "y": start["y"] + 1}, not_accessible_tiles, board_height, board_width, processed_tiles)
+    result = result + get_number_of_connected_tile_recursively({"x": start["x"] - 1, "y": start["y"]}, not_accessible_tiles, board_height, board_width, processed_tiles)
+    result = result + get_number_of_connected_tile_recursively({"x": start["x"], "y": start["y"] - 1}, not_accessible_tiles, board_height, board_width, processed_tiles)
+    return result
+
+
+def posible_move_to_index(possible_move: str, my_head: Dict[str, int]):
+    if possible_move == 'up':
+        return {"x": my_head["x"], "y": my_head["y"] + 1}
+    if possible_move == 'down':
+        return {"x": my_head["x"], "y": my_head["y"] - 1}
+    if possible_move == 'right':
+        return {"x": my_head["x"] + 1, "y": my_head["y"]}
+    if possible_move == 'left':
+        return {"x": my_head["x"] - 1, "y": my_head["y"]}
+
+
+def repriotize_preferred_directions_based_on_free_connected_tile(my_head: Dict[str, int], my_body: List[dict], other_snakes: List[dict], board_height: int, board_width: int, possible_moves_priotized: List[str]):
+    result_list = []
+    for move in possible_moves_priotized:
+        posible_move_index = posible_move_to_index(move, my_head)
+        number_of_connected_tile = get_number_of_connected_tile(posible_move_index, my_body, other_snakes, board_height, board_width)
+        print(f'number_of_connected_tile:{number_of_connected_tile}')
+        result_list.append({'move': move, 'number_of_connected_tile': number_of_connected_tile})
+    print(f'result_list:{result_list}')
+    result_list = sorted(result_list, key=lambda x: x['number_of_connected_tile'], reverse=True)
+    print(f'result_list:{result_list}')
+    result_list = list(map(lambda x: x['move'], result_list))
+    return result_list
+
+
+def choose_move(data: dict) -> str:
     """
-    This function is called everytime your snake is entered into a game.
-    request.json contains information about the game that's about to be played.
+    data: Dictionary of all Game Board data as received from the Battlesnake Engine.
+    For a full example of 'data', see https://docs.battlesnake.com/references/api/sample-move-request
+
+    return: A String, the single move to make. One of "up", "down", "left" or "right".
+
+    Use the information in 'data' to decide your next move. The 'data' variable can be interacted
+    with as a Python Dictionary, and contains all of the information about the Battlesnake board
+    for each move of the game.
+
     """
-    data = request.get_json()
+    my_head = data["you"]["head"]
+    my_body = data["you"]["body"]
 
-    print(f"{data['game']['id']} START")
-    return "ok"
+    possible_moves = ["up", "down", "left", "right"]
 
+    possible_moves = avoid_my_neck(my_head, my_body, possible_moves)
+    possible_moves = avoid_border(my_head, my_body, possible_moves, data['board']['height'], data['board']['width'])
+    if data['board']['snakes']:
+        for snake in data['board']['snakes']:
+            possible_moves = avoid_my_neck(my_head, snake['body'], possible_moves)
 
-@app.post("/move")
-def handle_move():
-    """
-    This function is called on every turn of a game. It's how your snake decides where to move.
-    Valid moves are "up", "down", "left", or "right".
-    """
-    data = request.get_json()
+    print(f'possible_moves:{possible_moves}')
+    if data['board']['food']:
+        print('food on board found')
+        nearest_food = get_nearest_food(my_head, data['board']['food'])
+        print(f'nearest_food:{nearest_food}')
+        possible_moves_priotized = get_preferred_directions_to_food(my_head, nearest_food, possible_moves)
+        print(f'possible_moves_priotized based on food:{possible_moves_priotized}')
+        possible_moves_priotized = repriotize_preferred_directions_based_on_free_connected_tile(my_head, my_body, data['board']['snakes'], data['board']['height'], data['board']['width'], possible_moves_priotized)
+        print(f'possible_moves_priotized based on free connectedtile:{possible_moves_priotized}')
+    move = possible_moves_priotized[0]
 
-    # TODO - look at the server_logic.py file to see how we decide what move to return!
-    move = server_logic.choose_move(data)
+    print(
+        f"{data['game']['id']} MOVE {data['turn']}: {move} picked from all valid options in {possible_moves_priotized}"
+    )
 
-    return {"move": move}
-
-
-@app.post("/end")
-def end():
-    """
-    This function is called when a game your snake was in ends.
-    It's purely for informational purposes, you don't have to make any decisions here.
-    """
-    data = request.get_json()
-
-    print(f"{data['game']['id']} END")
-    return "ok"
-
-
-if __name__ == "__main__":
-    logging.getLogger("werkzeug").setLevel(logging.ERROR)
-
-    print("Starting Battlesnake Server...")
-    port = int(os.environ.get("PORT", "8080"))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    return move
